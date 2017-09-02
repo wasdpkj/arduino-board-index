f18e4d16d4fd55cf182552b2fd5afdc73ed9c63e
老潘orz <wasdpkj@hotmail.com>
2017-09-03 03:40:31 +0800

    -B2

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ export AUX_PLATFORMS='declare -A aux_platforms=( [16MHzatmega328]="arduino:avr:u
 sleep 3
 export DISPLAY=:1.0
 
-echo -n "YLB-B1 "
+echo -e "\n\n\YLB-B2\n\n"
 
 echo "download and install arduino 1.8.3 YLB"
 wget https://downloads.arduino.cc/arduino-1.8.3-linux64.tar.xz
