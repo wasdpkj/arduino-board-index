c54800028d1a2eeecb984ad2ba9f3bf81fcb5c40
老潘orz <wasdpkj@hotmail.com>
2017-09-03 00:54:53 +0800

    1.8.4 哈哈aaa

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ export AUX_PLATFORMS='declare -A aux_platforms=( [16MHzatmega328]="arduino:avr:u
 sleep 3
 export DISPLAY=:1.0
 
-# download and install arduino 1.8.4
+# download and install arduino 1.8.4 pkj
 wget https://downloads.arduino.cc/arduino-1.8.4-linux64.tar.xz
 tar xf arduino-1.8.4-linux64.tar.xz
 mv arduino-1.8.4 $HOME/arduino_ide
