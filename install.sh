91b6ae09b7a66f9d463c88c8a2993b3e1193d869
老潘orz <wasdpkj@hotmail.com>
2017-09-02 23:25:15 +0800

    fix35

--- install.sh
+++ install.sh
@@ -52,8 +52,8 @@ DEPENDENCY_OUTPUT=$(arduino --pref "boardsmanager.additional.urls=https://github
 if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 
 echo -n "MICRODUINO AVR: "
-#DEPENDENCY_OUTPUT=$(arduino --install-boards microduino:avr 2>&1)
-DEPENDENCY_OUTPUT=$(arduino --install-boards "Microduino AVR Boards:avr")
+DEPENDENCY_OUTPUT=$(arduino --install-boards microduino:avr 2>&1)
+#DEPENDENCY_OUTPUT=$(arduino --install-boards "Microduino AVR Boards:avr")
 if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 
 # install random lib so the arduino IDE grabs a new library index
