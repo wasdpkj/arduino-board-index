c7574969a66af2b3524ee6d1d245ed78c9f264b6
老潘orz <wasdpkj@hotmail.com>
2017-09-03 03:11:41 +0800

    删除ada-A

--- install.sh
+++ install.sh
@@ -53,19 +53,19 @@ echo -e "\n#####################################################################
 echo "INSTALLING DEPENDENCIES"
 echo "########################################################################";
 
-echo -n "ADD PACKAGE INDEX-B: "
-DEPENDENCY_OUTPUT=$(arduino --pref "boardsmanager.additional.urls=https://github.com/wasdpkj/arduino-board-index/raw/gh-pages/package_microduino_index.json" --save-prefs 2>&1)
+#echo -n "ADD PACKAGE INDEX-B: "
+#DEPENDENCY_OUTPUT=$(arduino --pref "boardsmanager.additional.urls=https://github.com/wasdpkj/arduino-board-index/raw/gh-pages/package_microduino_index.json" --save-prefs 2>&1)
 #DEPENDENCY_OUTPUT=$(arduino --pref "boardsmanager.additional.urls=https://github.com/wasdpkj/arduino-board-index/raw/gh-pages/package_adafruit_index.json" --save-prefs 2>&1)
-if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
+#if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 
-echo -n "MICRODUINO AVR: "
-DEPENDENCY_OUTPUT=$(arduino --install-boards microduino:avr 2>&1)
+#echo -n "MICRODUINO AVR: "
+#DEPENDENCY_OUTPUT=$(arduino --install-boards microduino:avr 2>&1)
 #DEPENDENCY_OUTPUT=$(arduino --install-boards "Microduino AVR Boards:avr")
-if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
+#if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
 
 
 # install the due, esp8266, and microduino board packages
-echo -n "ADD PACKAGE INDEX-A: "
+echo -n "ADD PACKAGE INDEX-A-1: "
 #DEPENDENCY_OUTPUT=$(arduino --pref "boardsmanager.additional.urls=https://github.com/wasdpkj/arduino-board-index/raw/gh-pages/for_travis/package_microduino_index.json" --save-prefs 2>&1)
 DEPENDENCY_OUTPUT=$(arduino --pref "boardsmanager.additional.urls=https://github.com/wasdpkj/arduino-board-index/raw/gh-pages/package_adafruit_index.json" --save-prefs 2>&1)
 if [ $? -ne 0 ]; then echo -e "\xe2\x9c\x96"; else echo -e "\xe2\x9c\x93"; fi
