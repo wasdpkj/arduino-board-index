2596982ffce4764910608d69a9622ec649cf53a6
老潘orz <wasdpkj@hotmail.com>
2017-09-03 04:52:04 +0800

    V17

--- install.sh
+++ install.sh
@@ -19,7 +19,7 @@ export AUX_PLATFORMS='declare -A aux_platforms=( [16MHzatmega328]="arduino:avr:u
 sleep 3
 export DISPLAY=:1.0
 
-echo -e "\n\n\nYLB-B16\n\n\n"
+echo -e "\n\n\nYLB-B17\n\n\n"
 
 echo "download and install arduino 1.8.4 YLB"
 wget https://downloads.arduino.cc/arduino-1.8.4-linux64.tar.xz
